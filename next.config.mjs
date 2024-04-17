fc5293406444dfb1a76979a192fa30f87d5413f8
RazvanBanateanu <banateanu.razvan1@gmail.com>
2024-04-17 14:16:12 +0300

    fc

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/BanateanuRazvan",
     output: "export",
-    reactStrictMode: true,
 };
 
 export default nextConfig;
